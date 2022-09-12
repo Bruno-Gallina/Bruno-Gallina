b2ebabd88b50ea21ad3a0cea62fa515c06291c72
Bruno Gallina <53091700+Bruno-Gallina@users.noreply.github.com>
2022-09-12 10:44:23 -0300

    Create 1096.java

---
+++ 1096.java
@@ -0,0 +1,14 @@
+public class bee1096 {
+
+    public static void main(String [] args) {
+        
+        int i = 1;
+        
+        while (i <= 9) {
+            for (int j =7; j > 4; j --) {
+                System.out.printf("I=%d J=%d\n", i, j);
+            }
+            i += 2;
+        }
+    }
+}
