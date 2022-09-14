825e20fd5f4a724cd8ecd18f9f7ac4e428d65495
Bruno Gallina <53091700+Bruno-Gallina@users.noreply.github.com>
2022-09-14 11:04:18 -0300

    Create 1114.java

---
+++ 1114.java
@@ -0,0 +1,16 @@
+import java.io.I0Exception;
+import java.util. Scanner;
+
+public class Main {
+
+    public static void main (String] angs) throws I0Exception (
+    Scanner sc = new Scanner (System.in) ;
+    int senha = sc.nextInt );
+    while ( senha != 2002) f
+        System. out .println("Senha Invalida")
+        senha= sc.nextInt();
+    }
+
+    System.out. printin( "Acesso Permitido");
+  }
+}
